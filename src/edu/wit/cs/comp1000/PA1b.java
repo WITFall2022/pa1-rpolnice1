4a54facb2db6855ca294ca0b612bc49c502aad80
349492 <349492@664-161480-LAB.schools.mybps.org>
2022-12-13 10:35:50 -0500

    this is my saved code

--- src/edu/wit/cs/comp1000/PA1b.java
+++ src/edu/wit/cs/comp1000/PA1b.java
@@ -1,11 +1,30 @@
 package edu.wit.cs.comp1000;
-
-// TODO: document this class
+import java.util.Scanner;
 public class PA1b {
 
-	// TODO: document this function
 	public static void main(String[] args) {
-		// TODO: write your code here
+		
+		Scanner input = new Scanner(System.in);  
+		
+		int inches;
+		System.out.printf("Enter number of inches :");
+	   inches = input.nextInt(); 
+	   
+	   
+	   int yards = inches/ 36;
+	   int inchesf = inches % 36;
+	   int feet = inchesf / 12;
+	   int inches1 = inches % 12;
+	   
+	   
+	   
+	   System.out.printf("Yards: %d%n", yards);
+	   System.out.printf("feet: %d%n", feet);
+	   System.out.printf("inches: %d%n", inches1);
+	   
+	   
+	   
 	}
-
+	 
 }
+
